9e1e5261a8c6b6132212126e0d134bf59c418340
ammarAltarawneh <tarawnhammar@gmail.com>
2023-06-19 22:53:43 +0300

    update

--- Lab03-Review/Program.cs
+++ Lab03-Review/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System;
 
 namespace Lab03_Review
 {
@@ -9,15 +10,62 @@ namespace Lab03_Review
             try
             {
                 // Challenge 1
-                Console.WriteLine("Please enter three numbers separated by spaces:");
-                string inputNum = Console.ReadLine();
-                int product = Product3Num(inputNum);
-                Console.WriteLine($"The product of these 3 numbers is: {product}");
+                //Console.WriteLine("Please enter three numbers separated by spaces:");
+                //string inputNum = Console.ReadLine();
+                //int product = Product3Num(inputNum);
+                //Console.WriteLine($"The product of these 3 numbers is: {product}");
 
                 // Challenge 2
-                CalculateAverage();
+                //CalculateAverage();
 
+                // Challenge 3
+                //PrintPattern();
 
+                // Challenge 4
+                //Console.Write("Please enter array elements you want to find most appears element: ");
+                //string? arr = Console.ReadLine()!;
+                //string[] arrElements = arr.Split(' ');
+
+                //int[] ints = new int[arrElements.Length];
+
+                //for (int i = 0; i < arrElements.Length; i++)
+                //{
+                  //  int.TryParse(arrElements[i], out ints[i]);
+
+                //}
+                //int MostAppearsNumber = MostAppearsNumberInArray(ints);
+                //Console.WriteLine($"Most Appears Number In This Array is {MostAppearsNumber}");
+
+                // Challenge 5
+                //Console.Write("Please enter array elements you want to find max value element: ");
+                //string? arr2 = Console.ReadLine()!;
+                //if (arr2.Length < 1) throw new Exception("The array cannot be null!");
+                //string[] arrElements2 = arr2.Split(' ');
+
+                //int[] ints2 = new int[arrElements2.Length];
+
+                //for (int i = 0; i < arrElements2.Length; i++)
+                //{
+                  //  int.TryParse(arrElements2[i], out ints2[i]);
+
+                //}
+              //  int maxValue = MaxValueInArray(ints2);
+                //Console.WriteLine(maxValue);
+
+                // Challenge 6
+                string path = "../../../TextFile1.txt";
+                
+                WriteWord(path);
+                ReadFileText(path);
+
+                // Challenge 7
+                ReadWord(path);
+
+                // Challenge 8
+                Console.WriteLine("Enter the word to remove:");
+                string wordToRemove = Console.ReadLine();
+                RemoveWordFromFile(path, wordToRemove);
+                Console.WriteLine("Word removed successfully.");
 
             }
             catch (Exception ex)
@@ -54,7 +102,8 @@ namespace Lab03_Review
             return product;
         }
 
-        static void CalculateAverage()
+
+        public static void CalculateAverage()
         {
             Console.WriteLine("Please enter a number between 2-10:");
             if (int.TryParse(Console.ReadLine(), out int number) && number >= 2 && number <= 10)
@@ -70,7 +119,7 @@ namespace Lab03_Review
                     else
                     {
                         Console.WriteLine("Invalid input. Please enter a positive number.");
-                        i--; // Repeat the current iteration
+                        i--; 
                     }
                 }
 
@@ -81,6 +130,134 @@ namespace Lab03_Review
             {
                 Console.WriteLine("Invalid input. Please enter a number between 2-10.");
             }
+        }
+
+
+        static void PrintPattern()
+        {
+            int size = 5;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = size - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static int MostAppearsNumberInArray(int[] numbers)
+        {
+            Dictionary<int, int> counts = new();
+
+            foreach (int number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            int maxCount = counts.Values.Max();
+            int mostFrequentNumber = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (counts[number] == maxCount)
+                {
+                    mostFrequentNumber = number;
+                    break;
+                }
+            }
+
+            return mostFrequentNumber;
+        }
+
+        public static int MaxValueInArray(int[] numbers)
+        {
+            if (numbers.Length <= 0)
+                throw new Exception("The array cannot be empty.");
+
+
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
+        }
+
+
+        static void ReadFileText(string path)
+        {
+            string dataFromFile = File.ReadAllText(path);
+
+
+            Console.WriteLine(dataFromFile);
+        }
+
+        static void WriteWord(string path)
+        {
+            Console.WriteLine("Enter a word:");
+            string word = Console.ReadLine();
+
+            File.AppendAllText(path, word + " ");
+        }
+
+        static void ReadWord(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            int i = 0;
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"{line} in {i}");
+                i++;
+            }
+        }
+
+        public static void RemoveWordFromFile(string path, string wordToRemove)
+        {
+           string[] lines = File.ReadAllLines(path);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == wordToRemove)
+                {
+                    lines[i] = string.Empty;
+                }
+            }
+
+            File.WriteAllLines(path, lines);
         }
     }
 }
--- ReviewChallengeTest/UnitTest1.cs
+++ ReviewChallengeTest/UnitTest1.cs
@@ -4,21 +4,44 @@ namespace ReviewChallengeTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Product3Num()
-        {
+            
             [Theory]
             [InlineData("3 4 5", 60)] //Input a string of numbers and it returns a product of all numbers
             [InlineData("2 3 2 8", 12)] //Input more than 3 numbers
             [InlineData("4 5", 0)] //Input of less than 3 numbers
             [InlineData("-2 -3 -4", -24)] //Can it handle negative numbers
             [InlineData("3 5 k", 15)]   //Can it handle not numbers elements
-            void Probuct3Num_Test(string input, int expected)
+            public void Probuct3Num_Test(string input, int expected)
             {
                 int result = Program.Product3Num(input);
 
                 Assert.Equal(expected, result);
             }
+
+        
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 2, 2, 3, 3, 3, 1, 1, 5, 5, 6, 7, 8, 2, 1, 1 }, 1)]
+        [InlineData(new[] { 8, 8, 8, 8, 8, 8 }, 8)]
+        [InlineData(new[] { 3, 4, 5, 6, 7, 8 }, 3)]
+        [InlineData(new[] { 12, 1, 5, 5, 10, 9, 9, 1 }, 1)]
+        public void MostAppearsNumberInArray_Test(int[] numbers, int expected)
+        {
+            int MostAppearsNumber = Program.MostAppearsNumberInArray(numbers);
+
+            Assert.Equal(expected, MostAppearsNumber);
+        }
+
+        [Theory]
+        [InlineData(new[] { 5, 25, 99, 123, 78, 96, 555, 108, 4 }, 555)]
+        [InlineData(new[] { 3, 1, -2, 2, 3, 5, -3, -7 }, 5)] //Negative numbers
+        [InlineData(new[] { 8, 8, 8, 8, 8, 8 }, 8)] //All values are the same
+        public void MaxValueInArrayValidAndNegatives_Test(int[] numbers, int expectedMaxValue)
+        {
+
+            int maxValue = Program.MaxValueInArray(numbers);
+
+            Assert.Equal(expectedMaxValue, maxValue);
         }
     }
 }
